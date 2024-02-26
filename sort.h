4cdf5221d44a43c365f05bc60372a7b8dfae67d1
mudimurtala <ibnmuhyideen95@gmail.com>
2024-02-26 17:20:10 +0100

    Add prototypes

--- sort.h
+++ sort.h
@@ -51,5 +51,9 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
