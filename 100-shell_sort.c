17e1162ffcbf2f720512fc95d00121e3b21962b4
mudimurtala <ibnmuhyideen95@gmail.com>
2024-02-26 12:14:20 +0100

    Edit this file

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,59 +1,14 @@
 #include "sort.h"
 
 /**
- * swapp - the positions of two elements into an array
- * @array: array
- * @item1: array element
- * @item2: array element
+ * shell_sort - Shell sort
+ * @array: Array to sort
+ * @size: Size of array
  */
-void swapp(int *array, int item1, int item2)
-{
-
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
-
-
-/**
- * shell_sort - function that sorts an array of integers in ascending
- * order using the Shell sort algorithm, using the Knuth sequence
- * @size: size of the array
- * @array: list with numbers
- */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i;
-
-	if (array == NULL || size < 2)
+	if (size < 2 || array == NULL || size == 0)
 		return;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	while (gap > 0)
-	{
-		for (i = gap; i < size; i++)
-		{
-			int temp = array[i];
-			size_t j = i;
-
-			while (j >= gap && array[j - gap] > temp)
-			{
-				swapp(array, j, j - gap);
-				j -= gap;
-			}
-
-			array[j] = temp;
-		}
-
-		if (gap != 1)
-			print_array(array, size);
-
-		gap /= 3;
-	}
-
+	print_array(array, size);
 }
